e26a3e5a38213e441f8fa7ffe049d1929b69f30b
Emman Celestino <emmanuel.celestino93@gmail.com>
2017-08-04 23:01:46 +0800

    Uncomment initial.js

--- resources/views/client/client_index.blade.php
+++ resources/views/client/client_index.blade.php
@@ -110,6 +110,6 @@
         <script src="{{ asset('js/jquery-3.2.1.min.js') }}"></script>
         <script src="{{ asset('js/materialize.min.js') }}"></script>
         <script src="{{ asset('js/main.js') }}"></script>
-        <!-- <script src="{{ asset('js/initial.js') }}"></script> -->
+        <script src="{{ asset('js/initial.js') }}"></script>
         @yield('js')
 </html>
